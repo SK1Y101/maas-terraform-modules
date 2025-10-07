a05c612054c89ab361368042930981467a851780
Jack Lloyd-Walters <jack.lloyd-walters@canonical.com>
2025-10-07 15:33:31 +0100

    use the right model

--- modules/maas-deploy/main.tf
+++ modules/maas-deploy/main.tf
@@ -77,7 +77,7 @@ resource "juju_integration" "maas_region_postgresql" {
 resource "terraform_data" "juju_wait_for_maas" {
   input = {
     model = (
-      juju_application.maas_region[0].model
+      juju_model.maas_model.name
     )
   }
 
